1b7ceff917ebdf9a4eb026f04e841855770364fa
imanisul <ianisul768@gmail.com>
2026-07-28 21:12:53 +0530

    chore: migrate to next-sitemap for sitemap and robots generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://quizkaal.in",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false,
+};
