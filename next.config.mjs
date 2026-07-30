e542ee2d10e771a1fc43447a6b46891a81cb64c0
imanisul <ianisul768@gmail.com>
2026-07-30 20:56:41 +0530

    Fix CSP for Google Analytics and add favicon.ico

--- next.config.mjs
+++ next.config.mjs
@@ -27,7 +27,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:;",
           },
           {
             key: 'Permissions-Policy',
