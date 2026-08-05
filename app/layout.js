1a08ad8d3f03aa97bf1635ba23a6d31b9b6699bf
imanisul <ianisul768@gmail.com>
2026-08-05 12:25:07 +0530

    fix(seo): add 192x192 favicon for google search compatibility

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,8 @@ export const metadata = {
       { url: '/favicon.ico', sizes: '32x32' },
       { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
       { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
     ],
     shortcut: "/favicon.ico",
     apple: [
